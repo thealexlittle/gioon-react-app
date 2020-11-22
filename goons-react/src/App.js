d0b6143404318e04f8ee6a2059a390b495626197
Alex Little <alittle0@stanford.edu>
2020-11-22 16:56:07 -0600

    made some changes

--- goons-react/src/App.js
+++ goons-react/src/App.js
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          #Goon
+          I made this on a new branch
         </p>
         <a
           className="App-link"
